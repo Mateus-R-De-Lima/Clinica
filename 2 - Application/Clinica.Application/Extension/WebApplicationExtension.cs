e9a9a761bf24e9c8897a556ae83c8f0af6a40349
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-12-14 19:41:00 -0300

    feat: criando classe de extensão responsável pelo builder da API e de outros projetos futuros

---
+++ 2 - Application/Clinica.Application/Extension/WebApplicationExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clinica.Application.Usuario.Extension
+{
+    public static class WebApplicationExtension
+    {
+        public static WebApplication WebApplicationExtensionBuilder(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+  
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+           
+            app.MapOpenApi();
+
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
