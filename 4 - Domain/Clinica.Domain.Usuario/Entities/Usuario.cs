2a86a5e7a9d13dc44063b7f76fa4bc60e4044e1f
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-12-14 22:20:19 -0300

    feat: Implementando entidade de usuário.

---
+++ 4 - Domain/Clinica.Domain.Usuario/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Clinica.Domain.Usuario.Entities
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Guid EmpresaId { get; set; }
+        public string Role { get; set; } = string.Empty;
+    }
+}
